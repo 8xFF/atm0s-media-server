cc789687194370e900e4fcb61912ac90be57e78c
Giang Minh <giangndm@ows.vn>
2023-11-03 13:59:21 +0700

    update xflv to 0.3.0

--- transports/rtmp/src/rtmp/video_convert.rs
+++ transports/rtmp/src/rtmp/video_convert.rs
@@ -25,19 +25,20 @@ impl RtmpH264ToMediaPacketH264 {
 
     pub fn push(&mut self, data: Bytes, ts_ms: u32) -> Option<()> {
         let data = BytesMut::from(&data as &[u8]);
-        let frame = self.demuxer.demux(ts_ms, data).ok()?;
-        log::debug!("on h264 flvdemux frame type: {} {} {}", frame.frame_type, self.seq_no, (ts_ms * 90) as u32);
+        if let Some(frame) = self.demuxer.demux(ts_ms, data).ok()? {
+            log::debug!("on h264 flvdemux frame type: {} {} {}", frame.frame_type, self.seq_no, (ts_ms * 90) as u32);
 
-        let pkts = self.rtp_packetizer.payload(1200, &frame.data.freeze()).ok()?;
-        for pkt in pkts {
-            let codec = PayloadCodec::H264(frame.frame_type == 1, H264Profile::P42001fNonInterleaved, None);
-            let mut media = MediaPacket::simple_video(codec, self.seq_no, ts_ms * 90, pkt.to_vec());
-            media.nackable = true;
-            self.outputs.push_back(media);
-            self.seq_no += 1;
-        }
-        if let Some(pkt) = self.outputs.back_mut() {
-            pkt.marker = true;
+            let pkts = self.rtp_packetizer.payload(1200, &frame.data.freeze()).ok()?;
+            for pkt in pkts {
+                let codec = PayloadCodec::H264(frame.frame_type == 1, H264Profile::P42001fNonInterleaved, None);
+                let mut media = MediaPacket::simple_video(codec, self.seq_no, ts_ms * 90, pkt.to_vec());
+                media.nackable = true;
+                self.outputs.push_back(media);
+                self.seq_no += 1;
+            }
+            if let Some(pkt) = self.outputs.back_mut() {
+                pkt.marker = true;
+            }
         }
         Some(())
     }
