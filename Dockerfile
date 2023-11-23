5a44a0b5a77e0defc1d60dc15d95a8ab5b8681b9
giangndm <45644921+giangndm@users.noreply.github.com>
2023-11-24 02:43:24 +0700

    fix: auto build releases and publish docker (#84)
    * chore: fixing auto build with github actions
    * fix: switched to custom str0m for building TODO: switch back to str0m when it released

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM ubuntu:22.04 as base
+ARG TARGETPLATFORM
+COPY . /tmp
+WORKDIR /tmp
+
+RUN echo $TARGETPLATFORM
+RUN ls -R /tmp/
+# move the binary to root based on platform
+RUN case $TARGETPLATFORM in \
+        "linux/amd64")  BUILD=x86_64-unknown-linux-gnu  ;; \
+        "linux/arm64")  BUILD=aarch64-unknown-linux-gnu  ;; \
+        *) exit 1 ;; \
+    esac; \
+    mv /tmp/$BUILD/atm0s-media-server-$BUILD /atm0s-media-server; \
+    chmod +x /atm0s-media-server
+
+FROM ubuntu:22.04
+
+RUN apt-get update \
+    && apt-get install -y libssl-dev libopus-dev libfdk-aac-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY --from=base /atm0s-media-server /atm0s-media-server
+
+ENTRYPOINT ["/atm0s-media-server"]
--- transports/sip/src/sip/utils.rs
+++ transports/sip/src/sip/utils.rs
@@ -1,5 +1,45 @@
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
+use rsip::headers::CallId;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct CallId2(pub String);
+
+impl From<&str> for CallId2 {
+    fn from(s: &str) -> Self {
+        CallId2(s.to_string())
+    }
+}
+
+impl From<String> for CallId2 {
+    fn from(s: String) -> Self {
+        CallId2(s)
+    }
+}
+
+impl From<CallId2> for String {
+    fn from(call_id: CallId2) -> Self {
+        call_id.0
+    }
+}
+
+impl From<&CallId2> for String {
+    fn from(call_id: &CallId2) -> Self {
+        call_id.0.clone()
+    }
+}
+
+impl CallId2 {
+    pub fn new() -> Self {
+        Self(generate_random_string(16))
+    }
+}
+
+impl From<CallId> for CallId2 {
+    fn from(call_id: CallId) -> Self {
+        Self(call_id.to_string())
+    }
+}
 
 pub fn generate_random_string(length: usize) -> String {
     let rng = thread_rng();
