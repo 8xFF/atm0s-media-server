e78517fc030c4333208d22ffaf2d8a53ff7a1849
giangndm <45644921+giangndm@users.noreply.github.com>
2024-01-26 19:12:51 +0700

    fix: webrtc sdk apis json parse error (#193)

--- servers/media-server/src/server/webrtc/rpc/http.rs
+++ servers/media-server/src/server/webrtc/rpc/http.rs
@@ -263,8 +263,8 @@ impl WebrtcHttpApis {
         }
 
         log::info!("[HttpApis] create Webrtc endpoint {}/{}", body.0.room, body.0.peer);
-        body.0.session_uuid = Some(data.1.generate_session_uuid());
-        body.0.ip_addr = ip_addr;
+        body.0.session_uuid = data.1.generate_session_uuid();
+        body.0.ip_addr = ip_addr.into();
         body.0.user_agent = user_agent;
 
         let (req, rx) = RpcReqResHttp::<WebrtcConnectRequest, WebrtcConnectResponse>::new(body.0);
