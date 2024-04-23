9185ac1eb22b25e200cb4f390ae4fc14d3fdc1d6
Giang Minh <giangndm@ows.vn>
2024-04-23 19:02:09 +0700

    fix clippy warns

--- packages/media_utils/src/f16.rs
+++ packages/media_utils/src/f16.rs
@@ -1,6 +1,6 @@
 /// F16u is low percison float, which encoded inside u16 for better performance and space
 /// F16u is unsigned type, if you need signed type it is F16i
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct F16u(u16);
 
 impl Into<f32> for F16u {
@@ -15,13 +15,19 @@ impl F16u {
     }
 }
 
+impl PartialOrd for F16u {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
 impl Ord for F16u {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.0.cmp(&other.0)
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct F16i(i16);
 
 impl Into<f32> for F16i {
@@ -33,5 +39,11 @@ impl Into<f32> for F16i {
 impl Ord for F16i {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.0.cmp(&other.0)
+    }
+}
+
+impl PartialOrd for F16i {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
     }
 }
