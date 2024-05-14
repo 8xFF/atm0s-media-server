89d969b7dabd524cf298b4149eddfb07f9e07353
Giang Minh <giangndm@ows.vn>
2024-05-15 01:29:46 +0700

    restore protobuf build script for adapting with new protoc version

--- packages/protocol/build.rs
+++ packages/protocol/build.rs
@@ -1,8 +1,7 @@
 use std::io::Result;
 
+use prost_build::compile_protos;
+
 fn main() -> Result<()> {
-    let mut prost_build = prost_build::Config::new();
-    // Enable a protoc experimental feature.
-    prost_build.protoc_arg("--experimental_allow_proto3_optional");
-    prost_build.compile_protos(&["./proto/shared.proto", "./proto/conn.proto", "./proto/features.proto", "./proto/gateway.proto"], &["./proto"])
+    compile_protos(&["./proto/shared.proto", "./proto/conn.proto", "./proto/features.proto", "./proto/gateway.proto"], &["./proto"])
 }
