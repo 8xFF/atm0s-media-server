7dc4d1c46070accafb22919ec2479d111ddc228f
Giang Minh <giangndm@ows.vn>
2024-05-16 07:44:02 +0700

    two nodes run script

--- bin/node1.sh
+++ bin/node1.sh
@@ -1,1 +1,1 @@
-RUST_LOG=info RUST_BACKTRACE=1 cargo run -- --http-port 3001 --node-id 1 --sdn-port 10001 media --allow-private-ip
+RUST_LOG=atm0s_sdn_network::features::pubsub=info RUST_BACKTRACE=1 cargo run -- --http-port 3001 --node-id 0 --sdn-port 10001 media --allow-private-ip
--- bin/node2.sh
+++ bin/node2.sh
@@ -1,1 +1,1 @@
-RUST_LOG=info RUST_BACKTRACE=1 cargo run -- --http-port 3002 --node-id 2 --sdn-port 10002 --seeds 1@/ip4/127.0.0.1/udp/10001 media --allow-private-ip
+RUST_LOG=atm0s_sdn_network::features::pubsub=info RUST_BACKTRACE=1 cargo run -- --http-port 3002 --node-id 1 --sdn-port 10002 --seeds 0@/ip4/127.0.0.1/udp/10001 media --allow-private-ip
