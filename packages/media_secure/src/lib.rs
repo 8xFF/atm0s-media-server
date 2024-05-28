9a48bf37f07d699cbdd6e36f7bb54cd737e051db
Giang Minh <giangndm@ows.vn>
2024-05-28 10:56:12 +0700

    test: jwt secure

--- packages/media_secure/src/lib.rs
+++ packages/media_secure/src/lib.rs
@@ -6,12 +6,13 @@ pub mod jwt;
 /// This interface is for validating and generating data in each edge node like media-node
 pub trait MediaEdgeSecure {
     fn decode_obj<O: Serialize + DeserializeOwned>(&self, _type: &'static str, data: &str) -> Option<O>;
-    fn encode_conn_id<C: Serialize + DeserializeOwned>(&self, conn: C, ttl_ms: u64) -> String;
+    fn encode_conn_id<C: Serialize + DeserializeOwned>(&self, conn: C, ttl_seconds: u64) -> String;
     fn decode_conn_id<C: Serialize + DeserializeOwned>(&self, data: &str) -> Option<C>;
 }
 
-/// This interface for generate signed data for gateway, like connect token
+/// This interface for generating signed data for gateway, like connect token
 pub trait MediaGatewaySecure {
     fn validate_app(&self, token: &str) -> bool;
-    fn encode_obj<O: Serialize + DeserializeOwned>(&self, _type: &'static str, ob: O, ttl_ms: u64) -> String;
+    fn encode_obj<O: Serialize + DeserializeOwned>(&self, _type: &'static str, ob: O, ttl_seconds: u64) -> String;
+    fn decode_conn_id<C: Serialize + DeserializeOwned>(&self, data: &str) -> Option<C>;
 }
