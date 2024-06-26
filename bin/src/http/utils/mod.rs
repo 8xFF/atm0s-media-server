d5a1ae317babc66aed39e6082e67b14d3f584d41
Giang Minh <giangndm@ows.vn>
2024-06-26 22:57:06 +0700

    fix typos

--- bin/src/http/utils/mod.rs
+++ bin/src/http/utils/mod.rs
@@ -1,13 +1,13 @@
-mod embeded_files;
+mod embedded_files;
 mod payload_protobuf;
 mod payload_sdp;
 mod remote_ip;
 mod token;
 mod user_agent;
 
+pub use embedded_files::*;
 pub use payload_protobuf::*;
 pub use payload_sdp::*;
 pub use remote_ip::*;
 pub use token::*;
 pub use user_agent::*;
-pub use embeded_files::*;
