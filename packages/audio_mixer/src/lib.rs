580e7ac86b6ad9cf4c65d287c7d711f6fb59fe1e
dhilipsiva <dhilipsiva@pm.me>
2024-07-08 08:46:15 +0530

    chore: add a TODO comment.

--- packages/audio_mixer/src/lib.rs
+++ packages/audio_mixer/src/lib.rs
@@ -132,6 +132,8 @@ impl<Src: Debug + Clone + Eq + Hash> AudioMixer<Src> {
         for (i, slot) in self.outputs.iter().enumerate() {
             if let Some(OutputSlotState { audio_level, source }) = slot {
                 if let Some((_, _, lowest_slot_audio_level)) = &mut lowest {
+                    // TODO: We need to process some case we have same audio_level. Just check with smaller only:
+                    // https://github.com/8xFF/atm0s-media-server/pull/328#discussion_r1667336073
                     if *audio_level <= *lowest_slot_audio_level {
                         lowest = Some((i, source.clone(), *audio_level));
                     }
