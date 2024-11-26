02e85b90e9a2f522ad565fa06314653627b267f0
Giang Minh <giangndm@ows.vn>
2024-11-27 02:35:08 +0700

    fix build release

--- packages/media_console_front/build.rs
+++ packages/media_console_front/build.rs
@@ -1,8 +1,8 @@
-use std::process::Command;
+use std::{env, process::Command};
 
 fn main() {
     // Build Vite project when compiling in release mode
-    if !cfg!(debug_assertions) {
+    if !cfg!(debug_assertions) && env::var("SKIP_BUILD_CONSOLE_FRONT").is_err() {
         Command::new("pnpm")
             .current_dir(format!("{}/react-app", env!("CARGO_MANIFEST_DIR")))
             .args(&["install"])
